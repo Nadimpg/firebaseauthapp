e20ef752b82f80261093d36a12add23b0a13a794
humayunkabir11 <pythonloverkabir11@gmail.com>
2023-08-25 18:28:19 +0600

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth_app/auth/sign_in/sign_in_screen.dart';
+import 'package:firebase_auth_app/auth/sign_up/sign_up_screen.dart';
+import 'package:firebase_auth_app/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Sign_in_screen(),
+    );
+  }
+}
+
